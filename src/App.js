2456aceeece8148d948034434ec77b2c1ba30e13
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-04-22 16:32:17 +0700

    Modal slice

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import { useEffect } from 'react';
 import Modal from './components/Modal';
 function App() {
   const { cartItems } = useSelector((state) => state.cart);
+  const { isOpen } = useSelector((state) => state.modal);
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
   return (
     <main>
-      <Modal />
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
